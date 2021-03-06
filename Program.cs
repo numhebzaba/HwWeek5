1b840253f576c854f6db9de88bf24bede71ce272
numhebzaba <num.ptp@gmail.com>
2021-03-06 21:28:26 +0700

    เสร็จ

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Homework_Workshop_Week5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mode = Console.ReadLine();
+            double positionX = double.Parse(Console.ReadLine());
+            double positionY = double.Parse(Console.ReadLine());
+
+            if (mode == "time" || mode == "price")
+            {
+                if (positionX >= 0)
+                {
+                    double t = Math.Sqrt(positionY)+1; //t ไม่น้อยกว่า 0
+                    double p = Math.Pow((positionX - 1),2);
+                    if(mode == "time")
+                    {
+                        Console.WriteLine("{0},{1}",positionX,p);
+                    }
+                    else if(mode == "price")
+                    {
+                        Console.WriteLine("{0},{1}", t, positionY);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid mouse position");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid mode");
+            }
+            Console.ReadLine();
+        }
+    }
+}
